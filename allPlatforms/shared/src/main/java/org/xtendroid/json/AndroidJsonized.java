4cc5acbb60b586e1e5ca961abcd5d05e0c9f8e50
Jasm Sison <jasm.sison@gmail.com>
2016-02-17 17:01:57 +0100

    Added stub for @AndroidJsonized annotation, because I don't want to modify the active annotation handler.

---
+++ allPlatforms/shared/src/main/java/org/xtendroid/json/AndroidJsonized.java
@@ -0,0 +1,12 @@
+package org.xtendroid.json;
+
+/**
+ * This is a stub
+ */
+
+public @interface AndroidJsonized {
+  /**
+   * value could be a url or a valid json object, e.g. '{"a" : "string", "b" : true, "c" : 48}'
+   */
+  public String value();
+}
