aab0638ba413180ec12ff0ae4ca19ecb3b3cc033
Jasm Sison <jasm.sison@gmail.com>
2016-02-27 12:18:08 +0100

    Hopefully this will do the trick

--- xtendAllPlatforms/run-me-first.sh
+++ xtendAllPlatforms/run-me-first.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+VERSION=1.0.1
+
 export J2OBJC_ROOT=~/j2objcDist
 mkdir -p $J2OBJC_ROOT; pushd $J2OBJC_ROOT
-curl -L https://github.com/google/j2objc/releases/download/0.9.8.2.1/j2objc-0.9.8.2.1.zip > j2objc-0.9.8.2.1.zip
-unzip j2objc-0.9.8.2.1.zip; popd
+curl -L https://github.com/google/j2objc/releases/download/${VERSION}/j2objc-${VERSION}.zip > j2objc-${VERSION}.zip
+unzip j2objc-${VERSION}.zip; popd
 test -e local.properties || touch local.properties
 sed -i '' '/j2objc.home/d' local.properties
-echo j2objc.home=$J2OBJC_ROOT/j2objc-0.9.8.2.1 >> local.properties
+echo j2objc.home=$J2OBJC_ROOT/j2objc-${VERSION} >> local.properties
