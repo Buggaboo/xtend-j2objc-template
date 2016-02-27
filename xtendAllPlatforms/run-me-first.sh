d5a54a76cb8afbfec43cd84e5c44ffededfd28b5
Jasm Sison <jasm.sison@gmail.com>
2016-02-27 17:09:58 +0100

    Attempting to upgrade gradle to 2.8, also use j2objc v1.0.1

--- xtendAllPlatforms/run-me-first.sh
+++ xtendAllPlatforms/run-me-first.sh
@@ -4,8 +4,9 @@ VERSION=1.0.1
 
 export J2OBJC_ROOT=~/j2objcDist
 mkdir -p $J2OBJC_ROOT; pushd $J2OBJC_ROOT
-curl -L https://github.com/google/j2objc/releases/download/${VERSION}/j2objc-${VERSION}.zip > j2objc-${VERSION}.zip
-unzip j2objc-${VERSION}.zip; popd
+#curl -L https://github.com/google/j2objc/releases/download/${VERSION}/j2objc-${VERSION}.zip > j2objc-${VERSION}.zip
+#unzip j2objc-${VERSION}.zip; popd
 test -e local.properties || touch local.properties
 sed -i '' '/j2objc.home/d' local.properties
 echo j2objc.home=$J2OBJC_ROOT/j2objc-${VERSION} >> local.properties
+export J2OBJC_HOME=$J2OBJC_ROOT/j2objc-${VERSION}
